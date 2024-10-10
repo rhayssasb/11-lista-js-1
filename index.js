3958ed5abd236e6dd5431c8cf116b45dcdfbfb4b
profrafamatos <rcmm@cesar.school>
2024-10-10 14:36:15 -0300

    Arquivos necessários para resolver a lista de exercícios

---
+++ index.js
@@ -0,0 +1,27 @@
+/**** Escreva o código abaixo 👇******/
+
+
+
+
+
+
+
+
+
+
+
+/**** Escreva o código acima ☝️******/
+
+//Não altere as linhas abaixo
+module.exports = {
+  somar,
+  multiplicar,
+  subtrair,
+  dividir,
+  calcularMedia,
+  elevarPotencia,
+  calcularAreaRetangulo,
+  verificarParOuImpar,
+  calcularRaizQuadrada,
+  calcularHipotenusa,
+};
